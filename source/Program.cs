f14a4d3ba5f7e0c8e614805bee6a6afe8e7cbdd7
Alejandro M. Pi Cano <AlejandroPiCano@users.noreply.github.com>
2022-11-06 14:30:01 +0100

    First Commit

---
+++ source/Program.cs
@@ -0,0 +1,69 @@
+﻿using StackExchange.Redis;
+using System.Net;
+using UsingRedisConsoleApp;
+
+namespace source
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key, value;
+            var redisDB = RedisDB.Connection.GetDatabase();
+
+            // Setting the keys/values
+            Console.WriteLine("Please enter the keys/values or type 'Exit' for finish");
+
+            while (true)
+            {
+                Console.WriteLine("Please enter the key");
+                key = Console.ReadLine();
+
+                if (key.ToLower().Contains("exit"))
+                    break;
+
+                Console.WriteLine("Please enter the value");
+                value = Console.ReadLine();
+
+                if (key.ToLower().Contains("exit"))
+                    break;
+
+                redisDB.StringSet(key, value);
+            }
+
+            // Get the value
+            while (true)
+            {
+                Console.WriteLine("Please enter the key for get the value or type 'Exit' for finish");
+                key = Console.ReadLine();
+
+                if (key.ToLower().Contains("exit"))
+                    break;
+
+                value = redisDB.StringGet(key);
+
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("The value can not found");
+                else
+                    Console.WriteLine($"The value is {value}");                             
+            }
+
+            Console.WriteLine("Type 'keys' for get all the keys");
+            key = Console.ReadLine();
+
+            // Get all keys
+            if (key.ToLower().Contains("keys"))
+            {
+                EndPoint endPoint = RedisDB.Connection.GetEndPoints().First();
+                RedisKey[] keys = RedisDB.Connection.GetServer(endPoint).Keys(pattern: "*").ToArray();
+
+                foreach (var redisKey in keys)
+                {
+                    Console.WriteLine(redisKey);
+                } 
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/RedisDB.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingRedisConsoleApp
+{
+    internal static class RedisDB
+    {
+        private static Lazy<ConnectionMultiplexer> lazyConnnection = new Lazy<ConnectionMultiplexer>(() =>
+                                     ConnectionMultiplexer.Connect("localhost"));
+
+        public static ConnectionMultiplexer Connection { get => lazyConnnection.Value; }      
+    }
+}
